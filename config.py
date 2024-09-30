282d69740aee0eceb3c4998916188da2071ffc2c
chai35217793 <149489960+chai35217793@users.noreply.github.com>
2024-09-30 15:02:38 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ class Config:
     API_HASH = os.environ.get("API_HASH", "d7fbe2036e9ed2a1468fad5a5584a255")       
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "7490926656:AAHG-oUUzGPony9xfyApSI0EbbymhneDU1k")
     BOT_SESSION = os.environ.get("BOT_SESSION", "forwardbot")
-    OWNER_ID = os.environ.get("OWNER_ID", "7170426058")                             
+    OWNER_ID = int(os.environ.get("OWNER_ID", "7170426058"))                             
     DATABASE_URI = os.environ.get("DATABASE_URI", "mongodb+srv://chaiwala:autqio99wvMJEr0l@cluster0.nupdo.mongodb.net/?retryWrites=true&w=majority")  
     DATABASE_NAME = os.environ.get("DATABASE_NAME", "Cluste0")
     COLLECTION_NAME = os.environ.get('COLLECTION_NAME', 'Data')
