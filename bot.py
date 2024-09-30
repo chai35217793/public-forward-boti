6085b8995011efe4ee3dedf8d65672d986d7d001
chai35217793 <149489960+chai35217793@users.noreply.github.com>
2024-09-30 16:18:54 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,10 +3,7 @@ from config import Config
 from config import LOGGER
 from user import User
 import pyromod.listen
-from pyrogram.enums import ParseMode  # Import ParseMode
-
-# Set the session value directly
-Config.BOT_SESSION = "BQFWHgUAkKYKqHTzQpzU7Ea4H0ct7aaDCCkAU9oIBgan6-s4YC_PylsYdMK4t46CY4c1CxuL4kfC2DxO7q4SmTOvW5TaHlDh7Ucq9P-MeqkrLCYHDqUjS0Vi8cgdM7UNObIUVIeKfi8knCZIeEI_bEm9KjdHajVfCDA7gci8kDAhTuV2tENG_GOT3NR9naeo551ZT2HUpgjM4Pl3dkKTPIzAHTx3AbnW_nFqfMC9q--8N-iSm3BRq7n4D4MIS5f-Mydu57ZdWODNESD28T1OotG9hCIzGqeCMe345jU5JdRq_CBujDuMrj3QdzHJznzIs5YMCPdgNxWwWpTHH6EMdwrv0S5W3gAAAAGrZATKAA"
+from pyrogram.enums import ParseMode
 
 class Bot(Client):
     USER: User = None
@@ -14,7 +11,7 @@ class Bot(Client):
 
     def __init__(self):
         super().__init__(
-            Config.BOT_SESSION,
+            "session.session",  # Use the session file here
             api_hash=Config.API_HASH,
             api_id=Config.API_ID,
             plugins={
